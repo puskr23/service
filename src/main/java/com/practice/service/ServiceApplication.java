4425cd251a446c8f8610179d5196fe9c7657fbfc
puskar <puskr23@gmail.com>
2020-09-15 10:32:33 +0545

    changes'

--- src/main/java/com/practice/service/ServiceApplication.java
+++ src/main/java/com/practice/service/ServiceApplication.java
@@ -34,7 +34,16 @@ public class ServiceApplication {
 			Map<String, Integer> attempts=details.AttemptsMap();
 		for (Mobileclubillingplans user:repo.getMobileAndClubs())
 		{
-			
+			int downloadhub=attempts.get("5815044032551KDS");
+			int getvidi=attempts.get("8991132354851KDS");;
+			int mobihub=attempts.get("2762212814451KDS");;
+			int mobiplanet=attempts.get("0654095798651KDS");;
+			int mobizone=attempts.get("7891132354851KDS");;
+			if(getvidi==0&&mobihub==0&&downloadhub==0&&mobiplanet==0&&mobizone==0)
+			{
+				logger.info("Attempts for every services is finsihed");
+				break;
+			}
 			 logger.info("CLUB: {} Number: {} Clubname: {}", user.getAclubunique(), user.getAunique(), clubs.get(user.getAclubunique()));
 			 int hit=attempts.get(user.getAclubunique());
 			 logger.info("Before Attempt:{}", hit);   
